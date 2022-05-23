bc93779554c73beed98e55c4957836674d27a4b1
Mateusz <>
2022-05-23 19:33:09 +0200

    refactor: cleanup main file

--- main.lua
+++ main.lua
@@ -1,9 +1,2 @@
--- local h = require("hours")
-local u = require("myTime.utils")
-
--- local out = u:readCsv("./spec/test_file.csv", ",")
--- print(tprint(out))
-
--- print(u:createLine({1,2,3}, ";"))
-u:removeLastRowCsv("spec/test_file.csv")
-
+local utils = require("myTime.utils")
+local args = require("myTime.cli")
